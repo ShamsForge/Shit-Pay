1ed31ee080bda6972f2af2958ad6b02294be843f
Shaurya Jain <103116023+ShamsForge@users.noreply.github.com>
2025-06-25 00:58:56 +0000

    Users Schema

--- Shit Payment/server/app/models/users.py
+++ Shit Payment/server/app/models/users.py
@@ -2,42 +2,29 @@ import datetime
 import uuid
 from enum import StrEnum
 
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String
 
 from .base import Base
 
 
-class IdentityVerificationStatus(StrEnum):
-  unverified = "unverified"
-  pending = "pending"
-  verified = "verified"
-  failed = "failed"
-
-def get_display_name(self) -> str:
-  return {
-      IdentityVerificationStatus.unverified: "Unverified",
-      IdentityVerificationStatus.pending: "Pending",
-      IdentityVerificationStatus.verified: "Verified",
-      IdentityVerificationStatus.failed: "Failed",
-  }[self]
-
-
 class User(Base):
   __tablename__ = 'users'
-  id = Column(Integer, autoincrement=True, nullable=False, unique=True, primary_key=True, init=False)
+  id = Column(Integer, autoincrement=True, nullable=False, unique=True,
+  primary_key=True, init=False)
   uuid = Column(default_factory=uuid.uuid4, primary_key=True, unique=True)
   shit_id = Column(String, index=True)
   email = Column(String, unique=True, index=True)
   username = Column(String, unique=True, index=True)
-  password = Column(String)
+  hashed_password = Column(String)
+  hashed_pin = Column(Integer, min=4, max=4)
   fullname = Column(String, nullable=True)
   country_code = Column(String)
   phone_number = Column(String)
   recovery_email = Column(String, nullable=True)
   created_at = Column(String, default=datetime.datetime.now(datetime.UTC))
   active = Column(Boolean, default=True)
-  superuser = Column(Boolean, default=False)
   email_verified = Column(Boolean, nullable=False, default=False)
-  verified = Column(get_display_name = None)
+  superuser = Column(Boolean, default=False)
+  is_deleted = Column(Boolean, nullable=False, default=False)
   
   
